5ceef47dc4685f4f715365ed03089fafb1a7b81d
Hector <barrioshector695@gmail.com>
2026-06-03 15:26:33 -0500

    Proyecto creado correctamente

---
+++ database/migrations/2026_06_02_225629_create_empleados_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('empleados', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->string('apellido');
+            $table->date('fecha_nacimiento');
+            $table->date('fecha_ingreso');
+            $table->float('salario', 8, 2);
+            $table->enum('estado',
+            [
+                'activo',
+                'inactivo',
+                'suspendido',
+                'despedido',
+            ])->default('activo');
+            $table->foreignId('cargo_id')
+            ->constrained()
+            ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('empleados');
+    }
+};
---
+++ database/migrations/2026_06_03_194957_create_funciones_cargos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('funciones_cargos', function (Blueprint $table) {
+            $table->id();
+            $table->text('descripcion_funcion');
+            $table->enum('estado',
+            [
+                'activo',
+                'inactivo',
+            ])->default('activo');  
+            $table->foreignId('cargo_id')
+                ->constrained()
+                ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('funciones_cargos');
+    }
+};
