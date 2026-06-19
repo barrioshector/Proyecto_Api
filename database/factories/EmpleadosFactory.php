a1581e64b87fda493ecf3fd5702df7002f842682
Hector <barrioshector695@gmail.com>
2026-06-19 14:27:47 -0500

    Error corregido en empleadosfactory

--- database/factories/EmpleadosFactory.php
+++ database/factories/EmpleadosFactory.php
@@ -26,7 +26,7 @@ class EmpleadosFactory extends Factory
             'fecha_nacimiento'=> $this->faker->date(),
             'fecha_ingreso'=> $this->faker->date(),
             'salario'=> $this->faker->randomFloat(2, 1000, 10000),
-            'estado'=> $this->faker->randomElement(['activo', 'inactivo', 'suspendido', 'despedido']),
+            'estado'=> $this->faker->randomElement(['activo', 'inactivo']),
             'cargo_id' => Cargos::query()->inRandomOrder()->value('id') ?? Cargo::factory(),
        
         ];
